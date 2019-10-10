69bd7af4151e5ced4de76005146f66e3e8e6a3e5
local <local@WMII-R9V49PJ2P2>
2019-10-10 11:17:48 +0200

    Zadanie

---
+++ Zad_8.py
@@ -0,0 +1,11 @@
+# http://tiny.cc/uwm_psi
+
+indeksy = (134524,134123,123543)
+studenci = ('Adam', 'Kronicki', 'Michał', 'Mroczkowski', 'Ada', 'Wieluńska')
+
+pierwszy = studenci[:2], indeksy[0]
+drugi = studenci[2:4], indeksy[1]
+trzeci = studenci[4:], indeksy[2]
+print(pierwszy)
+print(drugi)
+print(trzeci)
